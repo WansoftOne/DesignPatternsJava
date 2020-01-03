b709227565ba526c825277d08cd545b535d52639
WansoftOne <47927541+WansoftOne@users.noreply.github.com>
2020-01-03 16:24:53 -0600

    create class Test for Builder Pattern

--- src/com/wansoft/design/pattern/builder/builder.java
+++ src/com/wansoft/design/pattern/builder/builder.java
@@ -1,5 +1,16 @@
-public class test{
-  public static void main(String [] args){
-    System.out.println("hello wordl form github");
-  }
+package com.wansoft.design.pattern.builder;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		BankAcount object = new BankAcount.Builder(1235)
+				.atBranch("Springfield")
+				.openingBalance(100)
+				.atRate(2.5)
+				.build();
+	
+		System.out.println(object);
+
+	}
+  
 }
