ce3125bc3ee42c00af0e833ed94407faedcaae60
Wansoft <47927541+WansoftOne@users.noreply.github.com>
2020-01-09 14:23:21 -0600

    Create Dog class

---
+++ src/com/wansoft/design/pattern/factory/abstractt/example1/Dog.java
@@ -0,0 +1,18 @@
+package com.wansoft.pattern.factory.abstractt.example1;
+
+public class Dog implements Animal {
+
+	@Override
+	public String getAnimal() {
+		return "a Dog";
+	}
+	
+	@Override
+	public String getSound() {
+		return "wof wof";
+	}
+	
+	public void soyAlgo() {
+		
+	}
+}
