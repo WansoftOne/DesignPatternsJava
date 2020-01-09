2222ea2f6a0e1b83a546199c65195fad370c92a0
Wansoft <47927541+WansoftOne@users.noreply.github.com>
2020-01-09 14:22:20 -0600

    Create Color class

---
+++ src/com/wansoft/design/pattern/factory/abstractt/example1/Color.java
@@ -0,0 +1,10 @@
+package com.wansoft.pattern.factory.abstractt.example1;
+
+/**
+ * @author Juan Salinas Zavaleta
+ * color Factory
+ */
+public interface Color {
+	String getColor();
+	boolean isPrimary();
+}
