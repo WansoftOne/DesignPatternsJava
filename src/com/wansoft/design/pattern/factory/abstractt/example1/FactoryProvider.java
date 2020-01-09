dd16ba130efd05bf8b0a0a856d22b61dc9dc7215
Wansoft <47927541+WansoftOne@users.noreply.github.com>
2020-01-09 14:24:48 -0600

    Create FactoryProvider class

---
+++ src/com/wansoft/design/pattern/factory/abstractt/example1/FactoryProvider.java
@@ -0,0 +1,21 @@
+package com.wansoft.pattern.factory.abstractt.example1;
+
+public class FactoryProvider {
+	
+	public static AbstractFactory<?> getFactory(String key) {
+		/**
+		 * Register all factories for the abstract factory
+		 */
+		if("ANIMAL".equals(key)) {
+			return new AnimalFactory();
+		}else if("COLOR".equals(key)) {
+			return new ColorFactory();
+		}
+		/**
+		 * else if("other case"){
+		 * 	return new SomeThing();
+		 * }
+		 */
+		return null;
+	}
+}
