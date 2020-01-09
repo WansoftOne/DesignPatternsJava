a3441cc55eef5f9bf0b19e0d0b8c730575738f88
Wansoft <47927541+WansoftOne@users.noreply.github.com>
2020-01-09 14:21:01 -0600

    Create Animal interface

---
+++ src/com/wansoft/design/pattern/factory/abstractt/example1/Animal.java
@@ -0,0 +1,10 @@
+package com.wansoft.pattern.factory.abstractt.example1;
+
+/**
+ * @author Juan Salinas Zavaleta
+ * animal Factory
+ */
+public interface Animal {
+	String getAnimal();
+	String getSound();
+}
